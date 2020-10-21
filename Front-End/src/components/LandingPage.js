4213d024baaed694ffdfc2cb6bd8b577f98ebca7
cedric-winbush <cawinbushjr@gmail.com>
2020-10-21 17:31:55 -0500

    adds starter landing page

--- Front-End/src/components/LandingPage.js
+++ Front-End/src/components/LandingPage.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import "./LandingPage.css"
+import choreTracker from "../assets/images/washing-dishes.jpg";
+
 
 const LandingPage = () => {
   return (
     <div>
-      <h2 style={{color: "white"}}> 
+      <nav className="navbar">
+        <div className="logo">
+          Logo
+        </div>
+        <div className="avatar">
+          <p> sign-in / sign-up</p>
+        </div>
+      </nav>
+      <main>
+      <h1 className ="title"> 
         The Landing Page
-        </h2>
+        </h1>
+        <section className="hero__image">
+          <img src={choreTracker} alt=""/>
+        </section>
+        <section className= "cards__section">
+          
+
+        </section>
+      </main>
+     
     </div>
   )
 }
